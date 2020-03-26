22cdaf399c9655a7166c152e216768ed1bab642b
mrzhang3389 <zh3389@gmail.com>
2020-03-26 10:17:43 +0800

    feat: "modify data preprocessing stop words and add  jieba word code"

--- config.py
+++ config.py
@@ -6,19 +6,40 @@
 @文档说明:
 """
 import os
+import numpy as np
+import pandas as pd
 train_data_path = "./data/train_data.csv"  # 训练数据的路径
 column_names = ["data", "class"]
 wiki_zh_vec_path = "./data/wiki.zh.vec"  # 词嵌入向量的路径
 embedded_matrix_size = 10240  # 根据词频保留的词数,用于构建嵌入矩阵,并确定嵌入矩阵的大小
+new_word_list_path = "./data/find_new_word/new_word_list.txt"  # 保存发现的新词列表的路径
 
 
-# import numpy as np
-# # 计算模型输入长度
-# self.all_data['doc_len'] = self.all_data[self.data_column_name].apply(
-#     lambda words: len(words.split(" ")))  # 添加一列用于存储每条数据的长度
-# input_max_seq_len = np.round(self.all_data['doc_len'].mean() + self.all_data['doc_len'].std()).astype(int)  # 模型输入长度
-# print("模型输入长度为:{}".format(input_max_seq_len))
-input_max_seq_len = 512  # 输入数据的长度
+class Compute_input_max_seq_len():
+    # 计算模型输入长度
+    def __init__(self, data):
+        self.data = data
+        self.input_len_threshold = 256
+        self.default_inputmax_seq_len = 512
+        self.input_max_seq_len = self.get_input_max_seq_len()
+        print("模型输入长度为:{}".format(self.input_max_seq_len))
+
+    def compute_input_max_seq_len(self):
+        self.data['doc_len'] = self.data[column_names[0]].apply(lambda words: len(words.split(" ")))  # 添加一列用于存储每条数据的长度
+        input_max_seq_len = np.round(self.data['doc_len'].mean() + self.data['doc_len'].std()).astype(int)  # 模型输入长度
+        return input_max_seq_len
+
+    def get_input_max_seq_len(self):
+        input_max_seq_len = self.compute_input_max_seq_len()
+        if input_max_seq_len > self.input_len_threshold:
+            return input_max_seq_len
+        else:
+            return self.default_inputmax_seq_len
+
+
+data = pd.read_csv(train_data_path)
+compute_seq_len = Compute_input_max_seq_len(data)
+input_max_seq_len = compute_seq_len.input_max_seq_len
 
 
 class data_preprocessing_config():
@@ -26,7 +47,7 @@ class data_preprocessing_config():
         self.tokenizer_path = "./data/tokenizer.json"  # 根据现有数据和tf-idf逆文件频率生成的词频文件
         self.pad_train_data_path = "./data/pad_train_data.npy"  # 用于存储去停用词后的data npy文件
         self.pad_train_label_path = "./data/pad_train_label.npy"  # 用于存储去去停用词后的label npy文件
-        self.stopwordpath = "./data/stopwordlist/baidu.txt"  # 停用词列表路径
+        self.stopwordpath = "./data/stopwordlist/"  # 停用词列表路径
 
 
 class Model_train_parameter_config():
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -25,13 +25,14 @@ class Data_preprocessing():
         self.stopwordpath = self.config.stopwordpath
         self.embedded_matrix_size = config.embedded_matrix_size
         self.input_max_seq_len = config.input_max_seq_len
+        self.new_word_list = config.new_word_list_path
         '''data param init'''
         self.data_column_name = column_names[0]
         self.class_column_name = column_names[1]
         self.all_data = data[[self.data_column_name, self.class_column_name]]
         self.data = self.all_data[self.data_column_name]
         self.label = self.all_data[self.class_column_name]
-        #
+        # create train file
         if not os.path.exists(self.pad_train_data_path) or not os.path.exists(self.pad_train_label_path) or not os.path.exists(self.tokenizer_path):
             if os.path.exists(self.pad_train_data_path):
                 os.remove(self.pad_train_data_path)
@@ -39,8 +40,27 @@ class Data_preprocessing():
                 os.remove(self.pad_train_label_path)
             if os.path.exists(self.tokenizer_path):
                 os.remove(self.tokenizer_path)
-            self.remove_stop_word_list = self.get_remove_stop_word()
-        self.tokenizer = self.get_tokenizer()
+
+    def get_stopwords_list(self):
+        '''
+        :return: get stopwordpath all stop word
+        '''
+        print("self.stopwordpath:", self.stopwordpath)
+        self.stopwords = []
+        for item in os.listdir(self.stopwordpath):
+            self.stopwords += list(set([line.strip() for line in open(self.stopwordpath + item, 'r', encoding='utf-8').readlines()]))
+        return self.stopwords
+
+    def add_jieba_word(self):
+        '''
+        :return: add new word to jieba
+        '''
+        with open(self.new_word_list, "r") as file:
+            print("add jieba word...")
+            for word in tqdm(file.readlines()):
+                word = word.strip()
+                jieba.add_word(word)
+        return None
 
     def remove_stop_words(self, sentence, cut_all=True):
         '''
@@ -48,14 +68,17 @@ class Data_preprocessing():
         :param cut_all: 分词模式
         :return: 返回分词后的列表
         '''
-        stopwords = [line.strip() for line in open(self.stopwordpath, 'r', encoding='utf-8').readlines()]
         processed_docs_train = []
         # jieba.add_word("添加jieba不能分的词")
+        if os.path.exists(self.new_word_list):
+            self.add_jieba_word()
+        self.get_stopwords_list()
+        print("cut word...")
         for word_list in tqdm(sentence):
             outstr = []
             word = jieba.cut(word_list.strip().rstrip(), cut_all=cut_all)
             for i in word:
-                if i not in stopwords:
+                if i not in self.stopwords:
                     if i != '\t' and i != ' ':
                         outstr.append(i)
             processed_docs_train.append(" ".join(outstr))
@@ -67,6 +90,7 @@ class Data_preprocessing():
         :return: tokenizer
         '''
         if not os.path.exists(self.tokenizer_path):
+            self.remove_stop_word_list = self.get_remove_stop_word()
             tokenizer = text.Tokenizer(num_words=self.embedded_matrix_size, lower=False, char_level=False)
             tokenizer.fit_on_texts(self.remove_stop_word_list)
             tokenizer_json = tokenizer.to_json()
@@ -90,6 +114,7 @@ class Data_preprocessing():
     def read_train_data(self):
         if not os.path.exists(self.pad_train_data_path) or not os.path.exists(self.pad_train_label_path):
             # transformer train data and pad sequences
+            self.tokenizer = self.get_tokenizer()
             self.pad_data = self.tokenizer.texts_to_sequences(self.remove_stop_word_list)
             self.pad_data = sequence.pad_sequences(self.pad_data, maxlen=self.input_max_seq_len)
             np.save(self.pad_train_data_path, self.pad_data)
